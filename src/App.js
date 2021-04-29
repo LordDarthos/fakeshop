73a2147d7f2405ce2f19e4daa74fe23a3c5b98d4
LordDarthos <rldmonroy@gmail.com>
2021-04-29 06:31:56 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import Header from './containers/Header'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail';
+import ProductComponent from './containers/ProductComponent';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Header/>
+      <Switch>
+        <Route path="/" exact component={ProductListing}/>
+        <Route path="/product/:productId" exact component={ProductDetail}/>
+        {/* <Route path="/" exact component={ProductComponent}/> */}
+        <Route>404 Not Found</Route>
+      </Switch>
+    </Router>
     </div>
   );
 }
